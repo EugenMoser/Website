36128421864be96edcaf0a8fb588690dcc809141
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-07-08 21:25:03 +0100

    add strings for english site

---
+++ src/app/(portfolio)/language/de.js
@@ -0,0 +1,147 @@
+const strings = {
+  aboutMe: `Ich habe endlich meine Leidenschaft für die Webentwicklung gefunden und bin jetzt wie ein Kind in einem
+  Süßigkeitenladen - immer begierig, etwas Neues zu lernen! Mit blitzschnellem
+  Verständnis und erstklassigen Fähigkeiten in HTML5, CSS3, JavaScript, React, Next.js,
+  MongoDB, Git, GitHub, Azure DevOps, Node.js und SPFx tauche ich mühelos in
+  in bestehende Projekte ein und finde mich schnell in komplexem Code zurecht. Besonders stolz bin ich
+  auf meine Fähigkeiten als Bug-Sucher und Code-Verbesserer stolz. Mein nächstes Ziel? zu werden.
+  ein Frontend-Assistent zu werden und mein Backend-Wissen auszubauen. Visual Studio Code und
+  npm sind meine treuen Helfer auf diesem Weg.`,
+
+  resume: {
+    experience: [
+      {
+        period: `2023 - heute`,
+        position: `Junior Developer (Schwerpunkt Microsoft SharePoint)`,
+        company: `CONET Technologies Holding GmbH`,
+        details: [
+          'Entwicklung von SharePoint-Lösungen mit Framework SPFx',
+          'Einsatz von Power Automate und Power Apps',
+        ],
+      },
+      {
+        period: `2022 – 2022`,
+        position: `Trainee Web Development`,
+        company: `neue fische – School and Pool für Digital Talent`,
+        details: [
+          'Intensiv-Coding-Bootcamp in Vollzeit',
+          '540 Stunden Programmierpraxis mit Eigenentwicklung einer Web App als Abschlussarbeit',
+          'Angewandte Technologien und Tools: HTML5, CSS3, JavaScript, React, Next.js, Node.js, npm, Storybook, MongoDB, Git, GitHub',
+        ],
+      },
+      {
+        period: `2021 – 2022`,
+        position: `Chassis-Disponent`,
+        company: `Dethleffs GmbH & Co. KG`,
+        details: [
+          'Ermittlung des Bedarfs an Chassis',
+          'Bestellung der Chassis beim Hersteller',
+          'Chassis-Eingangsbuchung',
+        ],
+      },
+      {
+        period: `2021 – 2021`,
+        position: `Kaufmännischer Angestellter im Controlling`,
+        company: `Walter Seitz GmbH & Co. KG`,
+        details: [
+          'Aufbereitung von Kennzahlen für die Geschäfts- und Vertriebsleitung',
+          'Auswertungen für den Bereich Vertrieb und After-Sales',
+          'Entwicklung neuer Auswertungen in Excel',
+          'Monatliche Auswertung und Analyse des Betriebsergebnisses',
+        ],
+      },
+      {
+        period: `2006 – 2020`,
+        position: `Verkaufsabwicklung / Automobilkaufmann`,
+        company: `Weber + Seitz GmbH`,
+        details: [
+          'Externe und interne Koordination und Organisation im Vertrieb',
+          'Auftrags- und Rechnungserstellung für Fahrzeuge und Zubehör',
+          'Abwicklung von Finanzierungs- und Leasingverträgen',
+          'Verwaltung der Stammdaten und Fahrzeugdaten',
+          'Inserieren von Fahrzeugen auf diversen Online-Plattformen',
+          'Beauftragung und Organisation externer Dienstleister (Fotografen, Gutachter, Fahrzeugaufbereiter, Fahrzeugbekleber)',
+        ],
+      },
+    ],
+    continuingEducation: [
+      {
+        period: `2010 – 2013`,
+        position: `Geprüfter Wirtschaftsfachwirt (Bachelor Professional of Business)`,
+        company: `IHK Schwaben`,
+        details: [
+          'Betriebswirtschaftslehre',
+          'Volkswirtschaftslehre',
+          'Recht (BGB, HGB, Wettbewerbsrecht, Arbeitsrecht)',
+          'Betriebliches Rechnungswesen, Investition, Finanzierung und Controlling',
+          'Führung und Zusammenarbeit im Unternehmen',
+          'Betriebliches Management',
+          'Marketing und Vertrieb',
+          'Logistik',
+          'Steuer',
+        ],
+      },
+      {
+        period: `Oktober 2013`,
+        position: `Ausbildung der Ausbilder (AdA)`,
+        company: `IHK Schwaben`,
+        details: [
+          'Nachweis zur Vermittlung der berufs- und arbeitspädagogischen Fertigkeiten, Kenntnisse und Fähigkeiten nach dem Berufsbildungsgesetz',
+        ],
+      },
+    ],
+    vocationalEducation: [
+      {
+        period: `2003 – 2006`,
+        position: `Automobilkaufmann`,
+        company: `Humpisschule Ravensburg, Kaufmännische Berufsschule`,
+        details: [],
+      },
+    ],
+    communityService: [
+      {
+        period: `2002 – 2003`,
+        position: `Krankentransport`,
+        company: `KAP in Wangen`,
+        details: ['Ausbildung zum Rettungsdiensthelfer'],
+      },
+    ],
+    graduation: [
+      {
+        period: `2000 – 2002`,
+        position: `Fachabitur / Fachhochschulreife und Staatlich geprüfter Wirtschaftsassistent`,
+        company: `Berufliches Schulzentrum Wangen, Berufskolleg Wirtschaftsinformatik`,
+        details: [],
+      },
+    ],
+  },
+  certifications: [
+    {
+      name: `ITIL® 4 Foundation (PeopleCert)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1714120003654/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+    {
+      name: `Professional Scrum Master I (Scrum.org)`,
+      link: `https://www.credly.com/badges/209b3aa3-700c-44f4-b2fa-baf525ab6569/public_url`,
+    },
+    {
+      name: `Microsoft 365 Certified: Fundamentials (Microsoft)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1707384542261/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+  ],
+  education: [
+    {
+      name: `IT-Bootcamp (neuefische)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1720299471327/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+    {
+      name: `Bachelor Professional of Business (IHK Schwaben)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1720299399582/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+    {
+      name: `Ausbildung der Ausbilder (IHK Schwaben)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1720299352602/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+  ],
+};
+export { strings };
---
+++ src/app/(portfolio)/language/en.js
@@ -0,0 +1,151 @@
+const strings = {
+  aboutMe: `I've finally found my passion in web development and now I’m like a kid in a 
+  candy store – always eager to learn something new! With lightning-fast 
+  understanding and top-notch skills in HTML5, CSS3, JavaScript, React, Next.js, 
+  MongoDB, Git, GitHub, Azure DevOps, Node.js, and SPFx, I effortlessly dive into 
+  existing projects and quickly get up to speed with complex code. I’m especially 
+  proud of my bug-hunting and code-enhancing ninja skills. My next goal? Becoming 
+  a frontend wizard and building up my backend knowledge. Visual Studio Code and 
+  npm are my trusty sidekicks along the way.`,
+
+  resume: {
+    experience: [
+      {
+        period: '2023 - present',
+        position: 'Junior Developer (Focus on Microsoft SharePoint)',
+        company: 'CONET Technologies Holding GmbH',
+        details: [
+          'Development of SharePoint solutions with SPFx framework',
+          'Use of Power Automate and Power Apps',
+        ],
+      },
+      {
+        period: '2022 - 2022',
+        position: 'Trainee Web Development',
+        company: 'neue fische – School and Pool for Digital Talent',
+        details: [
+          'Intensive full-time coding bootcamp',
+          '540 hours of programming practice with the development of a web app as a final project',
+          'Technologies and tools used: HTML5, CSS3, JavaScript, React, Next.js, Node.js, npm, Storybook, MongoDB, Git, GitHub',
+        ],
+      },
+      {
+        period: '2021 - 2022',
+        position: 'Chassis Dispatcher',
+        company: 'Dethleffs GmbH & Co. KG',
+        details: [
+          'Determining the need for chassis',
+          'Ordering chassis from the manufacturer',
+          'Chassis receipt booking',
+        ],
+      },
+      {
+        period: '2021 - 2021',
+        position: 'Commercial Clerk in Controlling',
+        company: 'Walter Seitz GmbH & Co. KG',
+        details: [
+          'Preparation of key figures for management and sales management',
+          'Evaluations for the sales and after-sales department',
+          'Development of new evaluations in Excel',
+          'Monthly evaluation and analysis of operating results',
+        ],
+      },
+      {
+        period: '2006 - 2020',
+        position: 'Sales Processing / Automobile Salesman',
+        company: 'Weber + Seitz GmbH',
+        details: [
+          'External and internal coordination and organization in sales',
+          'Creation of orders and invoices for vehicles and accessories',
+          'Handling financing and leasing contracts',
+          'Management of master data and vehicle data',
+          'Advertising vehicles on various online platforms',
+          'Commissioning and organizing external service providers (photographers, appraisers, vehicle detailers, vehicle wrappers)',
+        ],
+      },
+    ],
+    continuingEducation: [
+      {
+        period: '2010 - 2013',
+        position:
+          'Certified Business Administrator (Bachelor Professional of Business)',
+        company: 'IHK Schwaben',
+        details: [
+          'Business administration',
+          'Economics',
+          'Law (BGB, HGB, competition law, labor law)',
+          'Business accounting, investment, financing, and controlling',
+          'Leadership and cooperation in the company',
+          'Business management',
+          'Marketing and sales',
+          'Logistics',
+          'Taxes',
+        ],
+      },
+      {
+        period: 'October 2013',
+        position: 'Training of Trainers (AdA)',
+        company: 'IHK Schwaben',
+        details: [
+          'Proof of ability to impart vocational and work pedagogical skills, knowledge, and abilities according to the Vocational Training Act',
+        ],
+      },
+    ],
+    vocationalEducation: [
+      {
+        period: '2003 - 2006',
+        position: 'Automobile Salesman',
+        company: 'Humpisschule Ravensburg, Commercial Vocational School',
+        details: [],
+      },
+    ],
+    communityService: [
+      {
+        period: '2002 - 2003',
+        position: 'Patient Transport',
+        company: 'KAP in Wangen',
+        details: ['Training as a paramedic'],
+      },
+    ],
+    graduation: [
+      {
+        period: '2000 - 2002',
+        position:
+          'University Entrance Qualification / State-Certified Business Assistant',
+        company:
+          'Vocational School Center Wangen, Vocational College for Business Informatics',
+        details: [],
+      },
+    ],
+  },
+
+  certifications: [
+    {
+      name: `ITIL® 4 Foundation (PeopleCert)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1714120003654/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+    {
+      name: `Professional Scrum Master I (Scrum.org)`,
+      link: `https://www.credly.com/badges/209b3aa3-700c-44f4-b2fa-baf525ab6569/public_url`,
+    },
+    {
+      name: `Microsoft 365 Certified: Fundamentials (Microsoft)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1707384542261/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+  ],
+  education: [
+    {
+      name: `IT-Bootcamp (neuefische)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1720299471327/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+    {
+      name: `Bachelor Professional of Business (IHK Schwaben)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1720299399582/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+    {
+      name: `Train the Trainer (IHK Schwaben)`,
+      link: `https://www.linkedin.com/in/eugen-moser/details/certifications/1720299352602/single-media-viewer/?profileId=ACoAACviTdMBMDUILrpUMpI-0E_J4J6x82fs4CY`,
+    },
+  ],
+};
+export { strings };
